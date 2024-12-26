28fd9f06ec2b61e0f96d1e36a4e147b297db3bf3
sung <63391638+sung01299@users.noreply.github.com>
2024-12-26 23:17:22 +0900

    added join test

--- src/main/java/io/github/sung01299/howmuch/domain/user/service/UserServiceImpl.java
+++ src/main/java/io/github/sung01299/howmuch/domain/user/service/UserServiceImpl.java
@@ -2,12 +2,10 @@ package io.github.sung01299.howmuch.domain.user.service;
 
 import io.github.sung01299.howmuch.domain.user.entity.User;
 import io.github.sung01299.howmuch.domain.user.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
--- src/test/java/io/github/sung01299/howmuch/user/UserServiceImplTest.java
+++ src/test/java/io/github/sung01299/howmuch/user/UserServiceImplTest.java
@@ -7,10 +7,12 @@ import io.github.sung01299.howmuch.domain.user.service.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(UserServiceImpl.class)
 public class UserServiceImplTest {
 
@@ -24,7 +26,11 @@ public class UserServiceImplTest {
     void join() {
         User user = new User();
         user.setUserId(1L);
+        System.out.println("user = " + user);
 
         userService.join(user);
+
+//        User user1 = userRepository.findById(1L).get();
+//        System.out.println("user1 = " + user1);
     }
 }
