07e4b8605d612bb2c7a2a6fad8b5ace183de07a4
sung <63391638+sung01299@users.noreply.github.com>
2024-12-27 17:02:47 +0900

    user join test

--- src/main/java/io/github/sung01299/howmuch/domain/user/service/UserServiceImpl.java
+++ src/main/java/io/github/sung01299/howmuch/domain/user/service/UserServiceImpl.java
@@ -2,13 +2,17 @@ package io.github.sung01299.howmuch.domain.user.service;
 
 import io.github.sung01299.howmuch.domain.user.entity.User;
 import io.github.sung01299.howmuch.domain.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService {
 
+    @Autowired
     private final UserRepository userRepository;
 
     @Override
--- src/test/java/io/github/sung01299/howmuch/user/UserServiceImplTest.java
+++ src/test/java/io/github/sung01299/howmuch/user/UserServiceImplTest.java
@@ -4,12 +4,15 @@ import io.github.sung01299.howmuch.domain.user.entity.User;
 import io.github.sung01299.howmuch.domain.user.repository.UserRepository;
 import io.github.sung01299.howmuch.domain.user.service.UserService;
 import io.github.sung01299.howmuch.domain.user.service.UserServiceImpl;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
+import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -24,13 +27,18 @@ public class UserServiceImplTest {
 
     @Test
     void join() {
-        User user = new User();
-        user.setUserId(1L);
-        System.out.println("user = " + user);
+        User user1 = new User();
+        User user2 = new User();
 
-        userService.join(user);
+        System.out.println("user1 = " + user1);
+        System.out.println("user2 = " + user2);
 
-//        User user1 = userRepository.findById(1L).get();
-//        System.out.println("user1 = " + user1);
+        userService.join(user1);
+        userService.join(user2);
+
+        List<User> allUsers = userRepository.findAll();
+        for (User user: allUsers) {
+            System.out.println(user.getUserId());
+        }
     }
 }
